7d8b9cbf5f84ae6acb3ed3ce0d2084d6329c2b94
Stephen White <iamsilk3495@gmail.com>
2021-06-24 14:49:03 -0300

    Added Discord logging sink

---
+++ SilKsPlugins.DiscordBot.Logging/DiscordSink.cs
@@ -0,0 +1,59 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Serilog.Core;
+using Serilog.Events;
+using SilKsPlugins.DiscordBot.Logging.Configuration;
+using System;
+using System.Diagnostics;
+
+namespace SilKsPlugins.DiscordBot.Logging
+{
+    public class DiscordSink : ILogEventSink
+    {
+        private readonly IFormatProvider? _formatProvider;
+        private readonly DiscordSocketClient _discordClient;
+        private readonly IDiscordChannelLogConfigurer _discordChannels;
+
+        public DiscordSink(DiscordSocketClient discordClient,
+            IDiscordChannelLogConfigurer discordChannels)
+        {
+            _discordClient = discordClient;
+            _discordChannels = discordChannels;
+        }
+
+        public DiscordSink(IFormatProvider formatProvider,
+            DiscordSocketClient discordClient,
+            IDiscordChannelLogConfigurer discordChannels) : this(discordClient, discordChannels)
+        {
+            _formatProvider = formatProvider;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (_discordClient.ConnectionState != ConnectionState.Connected)
+            {
+                return;
+            }
+
+            var message = logEvent.RenderMessage(_formatProvider);
+
+            foreach (var channelId in _discordChannels.GetChannelIds())
+            {
+                try
+                {
+                    if (_discordClient.GetChannel(channelId) is not IMessageChannel channel)
+                    {
+                        Debug.WriteLine("Discord channel for logging does not exist: {0}", channelId);
+                        continue;
+                    }
+
+                    channel.SendMessageAsync(message);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Exception occurred when writing Discord log: {0}", ex);
+                }
+            }
+        }
+    }
+}
