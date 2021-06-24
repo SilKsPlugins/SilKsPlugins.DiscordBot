eb0a3b901196fad59224a98475d653b40b804558
Stephen White <iamsilk3495@gmail.com>
2021-06-23 21:58:49 -0300

    Made UserFriendlyException

---
+++ SilKsPlugins.DiscordBot/Commands/UserFriendlyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SilKsPlugins.DiscordBot.Commands
+{
+    public class UserFriendlyException : Exception
+    {
+        public UserFriendlyException()
+        {
+        }
+
+        public UserFriendlyException(string message) : base(message)
+        {
+        }
+    }
+}
