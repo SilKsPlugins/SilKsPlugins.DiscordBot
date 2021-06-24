e42835e16dc490239b75a51fe5fd40dd45bc4706
Stephen White <iamsilk3495@gmail.com>
2021-06-24 14:48:49 -0300

    Add IDiscordChannelLogConfigurer service

---
+++ SilKsPlugins.DiscordBot.Logging/Configuration/DiscordChannelLogConfigurer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SilKsPlugins.DiscordBot.Logging.Configuration
+{
+    public class DiscordChannelLogConfigurer : IDiscordChannelLogConfigurer
+    {
+        private readonly HashSet<ulong> _channelIds;
+
+        public DiscordChannelLogConfigurer()
+        {
+            _channelIds = new HashSet<ulong>();
+        }
+
+        public IReadOnlyCollection<ulong> GetChannelIds()
+        {
+            lock (_channelIds)
+            {
+                return _channelIds.ToList();
+            }
+        }
+
+        public bool AddChannel(ulong channelId)
+        {
+            lock (_channelIds)
+            {
+                return _channelIds.Add(channelId);
+            }
+        }
+
+        public bool RemoveChannel(ulong channelId)
+        {
+            lock (_channelIds)
+            {
+                return _channelIds.Remove(channelId);
+            }
+        }
+    }
+}
---
+++ SilKsPlugins.DiscordBot.Logging/Configuration/IDiscordChannelLogConfigurer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SilKsPlugins.DiscordBot.Logging.Configuration
+{
+    public interface IDiscordChannelLogConfigurer
+    {
+        IReadOnlyCollection<ulong> GetChannelIds();
+        
+        bool AddChannel(ulong channelId);
+        bool RemoveChannel(ulong channelId);
+    }
+}
