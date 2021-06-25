2c589421a267892ac4aff76bad4d65191330dbf4
Stephen White <iamsilk3495@gmail.com>
2021-06-24 22:37:54 -0300

    Created custom module base with ReplyAndDeleteAsync method

---
+++ SilKsPlugins.DiscordBot/Discord/CustomModuleBase.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using SilKsPlugins.DiscordBot.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace SilKsPlugins.DiscordBot.Discord
+{
+    public abstract class CustomModuleBase<T> : ModuleBase<T> where T : class, ICommandContext
+    {
+        private readonly CommandConfigAccessor _configuration;
+
+        protected CustomModuleBase(IServiceProvider serviceProvider)
+        {
+            _configuration = serviceProvider.GetRequiredService<CommandConfigAccessor>();
+        }
+
+        protected virtual async Task<IUserMessage> ReplyAndDeleteAsync(
+            string? message = null,
+            bool isTTS = false,
+            Embed? embed = null,
+            RequestOptions? options = null,
+            AllowedMentions? allowedMentions = null,
+            MessageReference? messageReference = null,
+            int? delay = null)
+        {
+            var reply = await ReplyAsync(message, isTTS, embed, options, allowedMentions, messageReference);
+
+            delay ??= _configuration.DeleteStandardReplyDelay;
+
+            await Task.Delay(delay.Value);
+
+            await reply.DeleteAsync();
+
+            return reply;
+        }
+    }
+}
