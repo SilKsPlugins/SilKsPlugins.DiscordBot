8b48140b9a8be1200ba94fe26a74b0feb5b95ca1
Stephen White <iamsilk3495@gmail.com>
2021-06-25 09:03:43 -0300

    Added Initial migration for role reactions database

---
+++ SilKsPlugins.DiscordBot/Migrations/RoleReactions/20210625120236_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SilKsPlugins.DiscordBot.Migrations.RoleReactions
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "RoleMessages",
+                columns: table => new
+                {
+                    GuildId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    ChannelId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    MessageId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    RoleId = table.Column<ulong>(type: "bigint unsigned", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleMessages", x => new { x.GuildId, x.ChannelId, x.MessageId });
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoleMessages");
+        }
+    }
+}
