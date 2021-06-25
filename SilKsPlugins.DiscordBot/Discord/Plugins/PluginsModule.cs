8183d8b7b0af68da0c222cfcea4cdad33276968c
Stephen White <iamsilk3495@gmail.com>
2021-06-25 10:48:42 -0300

    Removed unused contents of plugins module

--- SilKsPlugins.DiscordBot/Discord/Plugins/PluginsModule.cs
+++ SilKsPlugins.DiscordBot/Discord/Plugins/PluginsModule.cs
@@ -1,7 +1,10 @@
 ﻿using Discord;
 using Discord.Commands;
 using JetBrains.Annotations;
+using SilKsPlugins.DiscordBot.Commands;
 using SilKsPlugins.DiscordBot.Discord.Preconditions;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SilKsPlugins.DiscordBot.Discord.Plugins
@@ -9,19 +12,5 @@ namespace SilKsPlugins.DiscordBot.Discord.Plugins
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public class PluginsModule : ModuleBase<SocketCommandContext>
     {
-        [Command("sayembed")]
-        [Summary("Responds with an embed and deletes the original message.")]
-        [RequireBotAdmin]
-        public async Task SayEmbedAsync()
-        {
-            var embedBuilder = new EmbedBuilder();
-
-            var embed = embedBuilder.WithTitle("Test title")
-                .WithAuthor("Test author")
-                .WithDescription("Test description")
-                .Build();
-
-            await ReplyAsync(embed: embed);
-        }
     }
 }
