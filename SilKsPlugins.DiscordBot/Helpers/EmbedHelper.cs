528032901861828c93c08a702b9b6488b81d07f5
Stephen White <iamsilk3495@gmail.com>
2021-06-24 21:02:56 -0300

    Create embed helper

---
+++ SilKsPlugins.DiscordBot/Helpers/EmbedHelper.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+
+namespace SilKsPlugins.DiscordBot.Helpers
+{
+    public static class EmbedHelper
+    {
+        public static Embed SimpleEmbed(string message, Color? color = null)
+        {
+            var builder = new EmbedBuilder()
+                .WithTitle(message);
+
+            if (color != null)
+            {
+                builder.WithColor(color.Value);
+            }
+
+            return builder.Build();
+        }
+    }
+}
