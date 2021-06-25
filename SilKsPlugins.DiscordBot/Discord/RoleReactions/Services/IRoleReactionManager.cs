d1c0a6b13d98cf4d9c25c9b8d6c03223795ac575
Stephen White <iamsilk3495@gmail.com>
2021-06-24 21:20:45 -0300

    Created RoleReactionManager to employ RoleReactionsDbContext

---
+++ SilKsPlugins.DiscordBot/Discord/RoleReactions/Services/IRoleReactionManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace SilKsPlugins.DiscordBot.Discord.RoleReactions.Services
+{
+    public interface IRoleReactionManager
+    {
+        Task<ulong?> GetRoleForMessage(ulong channelId, ulong messageId);
+
+        Task<bool> AddRoleToMessage(ulong channelId, ulong messageId, ulong roleId);
+
+        Task<bool> RemoveRoleFromMessage(ulong channelId, ulong messageId);
+    }
+}
---
+++ SilKsPlugins.DiscordBot/Discord/RoleReactions/Services/RoleReactionManager.cs
@@ -0,0 +1,60 @@
+﻿using SilKsPlugins.DiscordBot.Databases.RoleReactions;
+using SilKsPlugins.DiscordBot.Databases.RoleReactions.Models;
+using System.Threading.Tasks;
+
+namespace SilKsPlugins.DiscordBot.Discord.RoleReactions.Services
+{
+    public class RoleReactionManager : IRoleReactionManager
+    {
+        private readonly RoleMessagesDbContext _dbContext;
+
+        public RoleReactionManager(RoleMessagesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> AddRoleToMessage(ulong channelId, ulong messageId, ulong roleId)
+        {
+            if (await _dbContext.RoleMessages.FindAsync(channelId, messageId) != null)
+            {
+                return false;
+            }
+
+            var roleMessage = new RoleMessage
+            {
+                ChannelId = channelId,
+                MessageId = messageId,
+                RoleId = roleId
+            };
+
+            await _dbContext.RoleMessages.AddAsync(roleMessage);
+
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> RemoveRoleFromMessage(ulong channelId, ulong messageId)
+        {
+            var roleMessage = await _dbContext.RoleMessages.FindAsync(channelId, messageId);
+
+            if (roleMessage == null)
+            {
+                return false;
+            }
+
+            _dbContext.RoleMessages.Remove(roleMessage);
+
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<ulong?> GetRoleForMessage(ulong channelId, ulong messageId)
+        {
+            var roleMessage = await _dbContext.RoleMessages.FindAsync(channelId, messageId);
+
+            return roleMessage?.RoleId;
+        }
+    }
+}
