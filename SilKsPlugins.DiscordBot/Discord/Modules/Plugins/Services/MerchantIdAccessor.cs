ec41c9e63cfe68b3bde1b4eba9554891951fbabf
Stephen White <iamsilk3495@gmail.com>
2021-06-29 07:56:51 -0300

    Added MerchantIdAccessor for merchant id persistance

---
+++ SilKsPlugins.DiscordBot/Discord/Modules/Plugins/Services/MerchantIdAccessor.cs
@@ -0,0 +1,58 @@
+﻿using SilKsPlugins.DiscordBot.Discord.Commands;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SilKsPlugins.DiscordBot.Discord.Modules.Plugins.Services
+{
+    public class MerchantIdAccessor
+    {
+        private const string MerchantIdFile = "merchantid.dat";
+
+        private readonly Runtime _runtime;
+
+        private Guid? _merchantId;
+
+        public MerchantIdAccessor(Runtime runtime)
+        {
+            _runtime = runtime;
+
+            Task.Run(async () =>
+            {
+                var path = GetMerchantIdPath();
+
+                if (File.Exists(path))
+                {
+                    var fileContents = await File.ReadAllTextAsync(path);
+
+                    if (Guid.TryParse(fileContents, out var merchantId))
+                    {
+                        _merchantId = merchantId;
+                    }
+                }
+            });
+        }
+
+        private string GetMerchantIdPath()
+        {
+            return Path.Combine(_runtime.WorkingDirectory, MerchantIdFile);
+        }
+
+        public Guid GetMerchantId() =>
+            GetMerchantIdNullable() ?? throw new UserFriendlyException("No merchant ID configured.");
+
+        public Guid? GetMerchantIdNullable()
+        {
+            return _merchantId;
+        }
+
+        public async Task SetMerchantId(Guid merchantId)
+        {
+            var path = GetMerchantIdPath();
+
+            await File.WriteAllTextAsync(path, merchantId.ToString());
+
+            _merchantId = merchantId;
+        }
+    }
+}
---
+++ SilKsPlugins.DiscordBot/Discord/Modules/Plugins/Services/PluginsServiceConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SilKsPlugins.DiscordBot.IoC;
+
+namespace SilKsPlugins.DiscordBot.Discord.Modules.Plugins.Services
+{
+    public class PluginsServiceConfigurator : IServiceConfigurator
+    {
+        public void ConfigureServices(ServiceConfiguratorContext context)
+        {
+            context.ServiceCollection.AddSingleton<MerchantIdAccessor>();
+        }
+    }
+}
