869141d9ae9595adebb999ed1dd639afc86f73d3
Stephen White <iamsilk3495@gmail.com>
2021-07-01 21:47:33 -0300

    Moved PluginsServiceConfigurator

--- SilKsPlugins.DiscordBot/Discord/Modules/Plugins/PluginsServiceConfigurator.cs
+++ SilKsPlugins.DiscordBot/Discord/Modules/Plugins/PluginsServiceConfigurator.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using SilKsPlugins.DiscordBot.Discord.Modules.Plugins.Services;
 using SilKsPlugins.DiscordBot.IoC;
 
-namespace SilKsPlugins.DiscordBot.Discord.Modules.Plugins.Services
+namespace SilKsPlugins.DiscordBot.Discord.Modules.Plugins
 {
+    [UsedImplicitly]
     public class PluginsServiceConfigurator : IServiceConfigurator
     {
         public void ConfigureServices(ServiceConfiguratorContext context)
